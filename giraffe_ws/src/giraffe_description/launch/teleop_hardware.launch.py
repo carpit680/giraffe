8457a86a827447163b80912a0437d34c3a61ef32
carpit680 <carpit680@gmail.com>
2024-12-24 09:29:22 +0530

    teleop tested with new hw interface

--- giraffe_ws/src/giraffe_description/launch/teleop_hardware.launch.py
+++ giraffe_ws/src/giraffe_description/launch/teleop_hardware.launch.py
@@ -1,101 +1,154 @@
-# Copyright 2024 Arpit Chauhan.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import os
-from os import pathsep
-
-from ament_index_python.packages import get_package_share_directory, get_package_prefix
-
-
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch.actions import AppendEnvironmentVariable
-
-from launch_ros.actions import Node
-
+import yaml
 import xacro
 
+from ament_index_python.packages import get_package_share_directory, get_package_share_path
 
+from launch import LaunchDescription
+from launch.actions import TimerAction, RegisterEventHandler
+from launch.substitutions import Command, PathJoinSubstitution, FindExecutable
+from launch.event_handlers import OnProcessExit
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+# LOAD FILE:
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return file.read()
+    except EnvironmentError:
+        # parent of IOError, OSError *and* WindowsError where available.
+        return None
+# LOAD YAML:
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:
+        # parent of IOError, OSError *and* WindowsError where available.
+        return None
+    
 def generate_launch_description():
-    # Launch Arguments
-    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            PathJoinSubstitution(
+                [
+                    FindPackageShare('giraffe_description'),
+                    'urdf',
+                    'giraffe_hardware.xacro.urdf',
+                ]
+            ),
+        ]
+    )
+    robot_description = {'robot_description': robot_description_content}
+    giraffe_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare('giraffe_description'),
+            'config',
+            'giraffe_controller.yaml',
+        ]
+    )
 
-    giraffe_description_path = os.path.join(
-        get_package_share_directory('giraffe_description'))
-    giraffe_description_share = get_package_prefix('giraffe_description')
-    model_path = os.path.join(giraffe_description_path, "models")
-    model_path += pathsep + os.path.join(giraffe_description_share, "share")
+    # node for controller manager which manages lifecycle of controllers defined for joints
+    control_node = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[robot_description, giraffe_controllers],
+        output={
+            'stdout': 'screen',
+            'stderr': 'screen',
+        },
+    )
 
-    env_var = AppendEnvironmentVariable('GZ_SIM_RESOURCE_PATH', model_path)
-    xacro_file = os.path.join(giraffe_description_path,
-                              'urdf',
-                              'giraffe.xacro.urdf')
-
-    doc = xacro.parse(open(xacro_file))
-    xacro.process_doc(doc)
-    params = {'robot_description': doc.toxml()}
-
-    node_robot_state_publisher = Node(
+    # node which publishes robot state including the robot description
+    robot_state_pub_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        output='screen',
-        parameters=[params]
+        output='both',
+        parameters=[robot_description],
     )
 
-    ignition_spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen',
-        arguments=['-string', doc.toxml(),
-                   '-name', 'giraffe',
-                   '-allow_renaming', 'true'],
+    # node to broadcasting joint states of various joints listed in urdf and configuration
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
     )
 
-    load_joint_state_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
+    # node to start controllers associated with the joints
+    arm_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['arm_controller', '--controller-manager', '/controller_manager'],
     )
 
-    load_arm_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'arm_controller'],
-        output='screen'
-    )
-    load_gripper_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'gripper_controller'],
-        output='screen'
+    gripper_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['gripper_controller', '--controller-manager', '/controller_manager'],
     )
 
-    # Bridge
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
-        output='screen'
-    )
+    # *** PLANNING CONTEXT *** #
+    # Robot description, SRDF:
+    robot_description_semantic_config = load_file("giraffe_moveit_config", "config/giraffe.srdf")
+    robot_description_semantic = {"robot_description_semantic": robot_description_semantic_config }
+    
+    # Kinematics.yaml file:
+    kinematics_yaml = load_yaml("giraffe_moveit_config", "config/kinematics.yaml")
+    robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
 
-    giraffe_driver = Node(
-        package="giraffe_control",
-        executable="giraffe_driver",
+    # Move group: OMPL Planning.
+    ompl_planning_pipeline_config = {
+        "move_group": {
+            "planning_plugin": "ompl_interface/OMPLPlanner",
+            "request_adapters": [
+                "default_planner_request_adapters/AddTimeOptimalParameterization",
+                "default_planner_request_adapters/FixWorkspaceBounds",
+                "default_planner_request_adapters/FixStartStateBounds",
+                "default_planner_request_adapters/FixStartStateCollision",
+                "default_planner_request_adapters/FixStartStatePathConstraints",
+            ],
+            "start_state_max_bounds_error": 0.1,
+        }
+    }
+
+    ompl_planning_yaml = load_yaml("giraffe_moveit_config", "config/ompl_planning.yaml")
+    ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
+
+    package_share = get_package_share_path('giraffe_description')
+    model_path = os.path.join(package_share, 'urdf', 'giraffe.urdf.xacro')
+    robot_description_config = xacro.process_file(model_path)
+    robot_description_config = robot_description_config.toxml()
+    robot_description_moveit = {'robot_description': robot_description_config}
+    moveit_config = (
+        MoveItConfigsBuilder("giraffe")
+        .robot_description(file_path="config/giraffe.urdf.xacro")
+        .robot_description_semantic(file_path="config/giraffe.srdf")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .to_moveit_configs()
+    )
+    # START NODE -> MOVE GROUP:
+    run_move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
         output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            {"use_sim_time": True},
+        ],
     )
+
     rviz_base = os.path.join(get_package_share_directory("giraffe_control"), "config")
     rviz_full_config = os.path.join(rviz_base, "teleop.rviz")
     rviz_node_full = Node(
@@ -105,34 +158,37 @@ def generate_launch_description():
         output="log",
         arguments=["-d", rviz_full_config],
     )
+
+    # delay start of robot_controller after `joint_state_broadcaster`
+    delay_motor_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[arm_controller_spawner, gripper_controller_spawner],
+        )
+    )
+
+    # delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node_full],
+        )
+    )
+
+    giraffe_driver = Node(
+        package='giraffe_control',
+        executable='giraffe_driver',
+        output='screen',
+    )
     return LaunchDescription([
-        env_var,
-        # Launch gazebo environment
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [os.path.join(get_package_share_directory('ros_ign_gazebo'),
-                              'launch', 'ign_gazebo.launch.py')]),
-            launch_arguments=[('gz_args', [' -r -v 4 empty.sdf'])]),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=ignition_spawn_entity,
-                on_exit=[load_joint_state_broadcaster],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=load_joint_state_broadcaster,
-                on_exit=[load_arm_controller, load_gripper_controller],
-            )
-        ),
-        node_robot_state_publisher,
-        ignition_spawn_entity,
-        # Launch Arguments
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value=use_sim_time,
-            description='If true, use simulated clock'),
-        bridge,
-        giraffe_driver,
-        rviz_node_full,
+        control_node,
+        robot_state_pub_node,
+        joint_state_broadcaster_spawner,
+        delay_rviz_after_joint_state_broadcaster_spawner,
+        delay_motor_controller_spawner_after_joint_state_broadcaster_spawner,
+        # TimerAction(
+        #     period=5.0,
+        #     actions=[run_move_group_node]
+        # ),
+        giraffe_driver
     ])
